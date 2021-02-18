67fbca6ae70730713413223ee04364eb44e1c37b
Andre Grossi <iamandregrossi@gmail.com>
2021-02-18 19:36:42 -0300

    Create info bar

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,33 @@
 <style lang="scss"  src="./styles/App.scss" > </style>
 
 <main>
-	<div class='top-section'> 
-		<h1>IP Address Tracker</h1>
-
-			<div class="input-group">
-				<input placeholder="Search for any IP address or domain"/>
-				<button type="submit">
-					<img src="/icons/icon-arrow.svg" alt="search" />
-				</button>
-			</div>
-	</div>
+		<div class='top-section'> 
+			<h1>IP Address Tracker</h1>
+	
+				<div class="input-group">
+					<input placeholder="Search for any IP address or domain"/>
+					<button aria-label="search">
+						<img src="/icons/icon-arrow.svg" alt="search" />
+					</button>
+				</div>
+	
+				<div class="info">
+					<div class="item">
+						<span>IP Address</span>
+						<p>192.212.174.101</p>
+					</div>
+					<div class="item">
+						<span>Location</span>
+						<p>Brooklyn, NY 10001</p>
+					</div>
+					<div class="item">
+						<span>Timezone</span>
+						<p>UTC -05:00</p>
+					</div>
+					<div class="item">
+						<span>ISP</span>
+						<p>SpaceX Starlink</p>
+					</div>
+				</div>
+		</div>
 </main>
