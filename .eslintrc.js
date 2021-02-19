1a4bd58d0c6d6b3ac7b5ef0d03effbb650752a2f
Andre Grossi <iamandregrossi@gmail.com>
2021-02-19 13:18:45 -0300

    Update ESLint + prettier rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,36 +1,40 @@
-const eslintSveltePreprocess = require("eslint-svelte3-preprocess");
-const path = require("path");
-const svelteConfigPath = path.resolve("./svelte.config");
+const eslintSveltePreprocess = require('eslint-svelte3-preprocess')
+const path = require('path')
+const svelteConfigPath = path.resolve('./svelte.config')
 
 module.exports = {
-	root: true,
-	env: {
-		node: true,
-		browser: true,
-	},
-	parser: "@typescript-eslint/parser",
-	parserOptions: {
-		createDefaultProgram: true,
-		ecmaVersion: 2019,
-		sourceType: "module",
-	},
-	extends: ["eslint:recommended"],
-	plugins: ["svelte3", "@typescript-eslint", "prettier"],
-	overrides: [
-		{
-			files: ["*.svelte"],
-			processor: "svelte3/svelte3",
-		},
-		{
-			files: ["*.ts", "*.json"],
-			extends: [
-				"plugin:@typescript-eslint/recommended",
-				"plugin:prettier/recommended",
-				"prettier/@typescript-eslint",
-			],
-		},
-	],
-	settings: {
-		"svelte3/preprocess": eslintSveltePreprocess(svelteConfigPath),
-	},
-};
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    createDefaultProgram: true,
+    ecmaVersion: 2019,
+    sourceType: 'module',
+  },
+  extends: [ 'eslint:recommended' ],
+  plugins: [ 'svelte3', '@typescript-eslint', 'prettier' ],
+  overrides: [
+    {
+      files: [ '*.svelte' ],
+      processor: 'svelte3/svelte3',
+    },
+    {
+      files: [ '*.ts', '*.json' ],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:prettier/recommended',
+        'prettier/@typescript-eslint',
+      ],
+    },
+  ],
+  rules: {
+    semi: [ 'error', 'never' ],
+    quotes: [ 'error', 'single' ],
+  },
+  settings: {
+    'svelte3/preprocess': eslintSveltePreprocess(svelteConfigPath),
+  },
+}
